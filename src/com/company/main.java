06da80337ea8ea33547572d591be42600c935751
Darkknight123 <philipkerry57@gmail.com>
2021-03-10 23:31:42 +0300

    using this

--- src/com/company/main.java
+++ src/com/company/main.java
@@ -2,8 +2,10 @@ package com.company;
 
 public class main {
     public static void main (String[] args){
-     Person p=new Person();
-     p.displayAge(30);
+     Mat m=new Mat();
+     m.multiply(10,20);
+    m.multiply(10,20,30);
+    m.multiply(10,20,30,40);
 
 
     }
