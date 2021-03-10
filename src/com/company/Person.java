2a58788a9ee5c40519137717cb4225762eb5138c
Darkknight123 <philipkerry57@gmail.com>
2021-03-10 23:24:50 +0300

    using this

---
+++ src/com/company/Person.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Person {
+    void displayAge(){
+        System.out.println("my age is 20");
+    }
+    void displayAge(int age){
+        System.out.println("my age is "+ age);
+    }
+    void displayAge(double age){
+        //System.out.println("my age is "+ age);
+    }
+}
--- src/com/company/main.java
+++ src/com/company/main.java
@@ -2,11 +2,8 @@ package com.company;
 
 public class main {
     public static void main (String[] args){
-        Student rob =new Student(2,80,70,60);
-        System.out.println(rob.Math);
-
-        Student John =new Student(1,90,56,90);
-        System.out.println(John.chem);
+     Person p=new Person();
+     p.displayAge(30);
 
 
     }
